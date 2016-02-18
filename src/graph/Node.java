f7d97d563e4ca92054110e57d3db33b6ad0c56c4
lvanparijs <lvanparijs@gmail.com>
2016-02-18 13:08:11 +0100

    Merge for Kareem <3
    nice

--- src/graph/Node.java
+++ src/graph/Node.java
@@ -1,5 +1,18 @@
 package graph;
 
+import java.util.ArrayList;
+
 public class Node {
 
+    private ArrayList<Edge> adjacent;
+    private int x,y;
+
+    public Node(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    private ArrayList<Edge> getAdjacent(){
+        return adjacent;
+    }
 }
--- src/map/Intersection.java
+++ src/map/Intersection.java
@@ -1,5 +1,36 @@
 package map;
 
-public abstract class Intersection {
+import graph.Node;
+import vehicle.Vehicle;
 
+import java.util.Queue;
+
+public abstract class Intersection extends Node{
+
+    private int x,y;
+    private Queue<Vehicle> qv;
+
+    private boolean isOpen(){
+
+    }
+
+    private int getTimerLength(){
+
+    }
+
+    private int getMaxFlow(){
+
+    }
+
+    private Queue<Vehicle> getQueue(Road r){
+
+    }
+
+    private int getX(){
+
+    }
+
+    private int getY(){
+
+    }
 }
