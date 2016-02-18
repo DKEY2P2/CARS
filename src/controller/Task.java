079908d012356b11b5b86ab208f8da7d0b42464f
Jonas Vacek <jvacek@me.com>
2016-02-18 12:49:38 +0100

    no message

--- src/controller/Task.java
+++ src/controller/Task.java
@@ -1,5 +1,9 @@
 package controller;
 
 public interface Task {
-
+    /**
+     * Takes care of advancing the vehicle and updating its many fields
+     * @return a confirmation of success or failure
+     */
+    public boolean update();
 }
--- src/vehicle/Vehicle.java
+++ src/vehicle/Vehicle.java
@@ -3,21 +3,23 @@ package vehicle;
 import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.ArrayList;
 
+import controller.Task;
 import map.Intersection;
 import map.Road;
+import ui.Drawable;
 
 /**
  * The abstract class for all vehicles and identities present in the simulation
  * 
  * @author jvacek
  */
-public abstract class Vehicle {
+public abstract class Vehicle implements Task, Drawable {
     /**
      * The current value of the vehicle's acceleration km/h
      */
     private double			       speed;
     /**
-     * The current value of the vehicle's acceleration in km/
+     * The current value of the vehicle's acceleration in m/s^2
      */
     private double			       acceleration;
     /**
@@ -49,8 +51,7 @@ public abstract class Vehicle {
      * road
      */
     private ArrayList<Intersection>	       traceLog;
-
-    
+					       
     /*
      * 
      * Constructor
@@ -59,7 +60,7 @@ public abstract class Vehicle {
     /**
      * The default constructor for the Vehicle abstract class
      */
-    public Vehicle(){
+    public Vehicle() {
 	setSpeed(0);
 	setAcceleration(0);
 	setDestination(null);
@@ -75,12 +76,7 @@ public abstract class Vehicle {
      * Update function
      * 
      */
-    /**
-     * The update function that determines
-     * @return
-     */
     public abstract boolean update();
-    
     
     /*
      * 
@@ -88,28 +84,37 @@ public abstract class Vehicle {
      * 
      */
     
+    
+    
+    
     /**
      * Adds an intersection to the trace log of the journey
+     * 
      * @param e the traversed Intersection
      */
-    public void addToTraceLog(Intersection e){
+    public void addToTraceLog(Intersection e) {
 	this.traceLog.add(e);
     }
+    
     /**
-     * Increases the amount of how much distance a vehicle has travelled by the amount x
-     * @param x the amount to increase by (negative for decrease) 
+     * Increases the amount of how much distance a vehicle has travelled by the
+     * amount x
+     * 
+     * @param x the amount to increase by (negative for decrease)
      */
-    public void plusDistance(double x){
+    public void plusDistance(double x) {
 	this.distance += x;
     }
+    
     /**
-     * Increases the amount of how much time a vehicle has spent on the road by the amount x
+     * Increases the amount of how much time a vehicle has spent on the road by
+     * the amount x
+     * 
      * @param x
      */
-    public void plusTimeOnRoad(double x){
-	this.timeOnRoad+=x;
+    public void plusTimeOnRoad(double x) {
+	this.timeOnRoad += x;
     }
-    
     
     /*
      * 
@@ -117,69 +122,67 @@ public abstract class Vehicle {
      * 
      */
     /**
-     *
      * @return
      */
     public double getSpeed() {
 	return this.speed;
     }
+    
     /**
-     * 
      * @return
      */
     public double getAcceleration() {
 	return this.acceleration;
     }
+    
     /**
-     * 
      * @return
      */
     public SimpleImmutableEntry<Road, Double> getDestination() {
 	return this.destination;
     }
+    
     /**
-     * 
      * @return
      */
     public SimpleImmutableEntry<Road, Double> getStart() {
 	return this.start;
     }
+    
     /**
-     * 
      * @return
      */
     public int getTimeOnRoad() {
 	return this.timeOnRoad;
     }
+    
     /**
-     * 
      * @return
      */
     public SimpleImmutableEntry<Road, Double> getPosition() {
 	return this.position;
     }
+    
     /**
-     * 
      * @return
      */
     public double getDesiredSpeed() {
 	return this.desiredSpeed;
     }
+    
     /**
-     * 
      * @return
      */
     public double getDistance() {
 	return this.distance;
     }
+    
     /**
-     * 
      * @return
      */
     public ArrayList<Intersection> getTraceLog() {
 	return this.traceLog;
     }
-    
     
     /*
      * 
@@ -187,63 +190,62 @@ public abstract class Vehicle {
      * 
      */
     /**
-     * 
      * @param speed
      */
     public void setSpeed(double speed) {
 	this.speed = speed;
     }
+    
     /**
-     * 
      * @param acceleration
      */
     public void setAcceleration(double acceleration) {
 	this.acceleration = acceleration;
     }
+    
     /**
-     * 
      * @param destination
      */
     public void setDestination(SimpleImmutableEntry<Road, Double> destination) {
 	this.destination = destination;
     }
+    
     /**
-     * 
      * @param start
      */
     public void setStart(SimpleImmutableEntry<Road, Double> start) {
 	this.start = start;
     }
+    
     /**
-     * 
      * @param timeOnRoad
      */
     public void setTimeOnRoad(int timeOnRoad) {
 	this.timeOnRoad = timeOnRoad;
     }
+    
     /**
-     * 
      * @param position
      */
     public void setPosition(SimpleImmutableEntry<Road, Double> position) {
 	this.position = position;
     }
+    
     /**
-     * 
      * @param desiredSpeed
      */
     public void setDesiredSpeed(double desiredSpeed) {
 	this.desiredSpeed = desiredSpeed;
     }
+    
     /**
-     * 
      * @param distance
      */
     public void setDistance(double distance) {
 	this.distance = distance;
     }
+    
     /**
-     * 
      * @param traceLog
      */
     public void setTraceLog(ArrayList<Intersection> traceLog) {
