a62de8da02d8c2e0e6a0444a66565058f75df315
imrayya <imrayya@gmail.com>
2016-02-18 13:28:42 +0100

    Start of drawing method of car

--- src/vehicle/Vehicle.java
+++ src/vehicle/Vehicle.java
@@ -4,24 +4,27 @@ import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.ArrayList;
 
 import controller.Task;
+import java.awt.Graphics;
+import java.awt.Rectangle;
 import map.Intersection;
 import map.Road;
 import ui.Drawable;
 
 /**
  * The abstract class for all vehicles and identities present in the simulation
- * 
+ *
  * @author jvacek
  */
 public abstract class Vehicle implements Task, Drawable {
+
     /**
      * The current value of the vehicle's acceleration km/h
      */
-    private double			       speed;
+    private double speed;
     /**
      * The current value of the vehicle's acceleration in m/s^2
      */
-    private double			       acceleration;
+    private double acceleration;
     /**
      * The place the vehicle wants to get to
      */
@@ -33,7 +36,7 @@ public abstract class Vehicle implements Task, Drawable {
     /**
      * The time since the vehicle has left the start point
      */
-    private int				       timeOnRoad;
+    private int timeOnRoad;
     /**
      * The current position of the vehicle on a road
      */
@@ -41,17 +44,17 @@ public abstract class Vehicle implements Task, Drawable {
     /**
      * The speed the vehicle driver would like to achieve
      */
-    private double			       desiredSpeed;
+    private double desiredSpeed;
     /**
      * The amount of distance travelled during the journey
      */
-    private double			       distance;
+    private double distance;
     /**
      * Keeps track of all the intersections that a vehicle has passed while on
      * road
      */
-    private ArrayList<Intersection>	       traceLog;
-					       
+    private ArrayList<Intersection> traceLog;
+
     /*
      * 
      * Constructor
@@ -61,61 +64,57 @@ public abstract class Vehicle implements Task, Drawable {
      * The default constructor for the Vehicle abstract class
      */
     public Vehicle() {
-	setSpeed(0);
-	setAcceleration(0);
-	setDestination(null);
-	setStart(null);
-	setTimeOnRoad(0);
-	setPosition(getStart());
-	setDesiredSpeed(0);
-	setDistance(0);
+        setSpeed(0);
+        setAcceleration(0);
+        setDestination(null);
+        setStart(null);
+        setTimeOnRoad(0);
+        setPosition(getStart());
+        setDesiredSpeed(0);
+        setDistance(0);
     }
-    
+
     /*
      * 
      * Update function
      * 
      */
     public abstract boolean update();
-    
+
     /*
      * 
      * Fun stuff
      * 
      */
-    
-    
-    
-    
     /**
      * Adds an intersection to the trace log of the journey
-     * 
+     *
      * @param e the traversed Intersection
      */
     public void addToTraceLog(Intersection e) {
-	this.traceLog.add(e);
+        this.traceLog.add(e);
     }
-    
+
     /**
      * Increases the amount of how much distance a vehicle has travelled by the
      * amount x
-     * 
+     *
      * @param x the amount to increase by (negative for decrease)
      */
     public void plusDistance(double x) {
-	this.distance += x;
+        this.distance += x;
     }
-    
+
     /**
      * Increases the amount of how much time a vehicle has spent on the road by
      * the amount x
-     * 
+     *
      * @param x
      */
     public void plusTimeOnRoad(double x) {
-	this.timeOnRoad += x;
+        this.timeOnRoad += x;
     }
-    
+
     /*
      * 
      * Getters
@@ -125,65 +124,65 @@ public abstract class Vehicle implements Task, Drawable {
      * @return
      */
     public double getSpeed() {
-	return this.speed;
+        return this.speed;
     }
-    
+
     /**
      * @return
      */
     public double getAcceleration() {
-	return this.acceleration;
+        return this.acceleration;
     }
-    
+
     /**
      * @return
      */
     public SimpleImmutableEntry<Road, Double> getDestination() {
-	return this.destination;
+        return this.destination;
     }
-    
+
     /**
      * @return
      */
     public SimpleImmutableEntry<Road, Double> getStart() {
-	return this.start;
+        return this.start;
     }
-    
+
     /**
      * @return
      */
     public int getTimeOnRoad() {
-	return this.timeOnRoad;
+        return this.timeOnRoad;
     }
-    
+
     /**
      * @return
      */
     public SimpleImmutableEntry<Road, Double> getPosition() {
-	return this.position;
+        return this.position;
     }
-    
+
     /**
      * @return
      */
     public double getDesiredSpeed() {
-	return this.desiredSpeed;
+        return this.desiredSpeed;
     }
-    
+
     /**
      * @return
      */
     public double getDistance() {
-	return this.distance;
+        return this.distance;
     }
-    
+
     /**
      * @return
      */
     public ArrayList<Intersection> getTraceLog() {
-	return this.traceLog;
+        return this.traceLog;
     }
-    
+
     /*
      * 
      * Setters
@@ -193,63 +192,73 @@ public abstract class Vehicle implements Task, Drawable {
      * @param speed
      */
     public void setSpeed(double speed) {
-	this.speed = speed;
+        this.speed = speed;
     }
-    
+
     /**
      * @param acceleration
      */
     public void setAcceleration(double acceleration) {
-	this.acceleration = acceleration;
+        this.acceleration = acceleration;
     }
-    
+
     /**
      * @param destination
      */
     public void setDestination(SimpleImmutableEntry<Road, Double> destination) {
-	this.destination = destination;
+        this.destination = destination;
     }
-    
+
     /**
      * @param start
      */
     public void setStart(SimpleImmutableEntry<Road, Double> start) {
-	this.start = start;
+        this.start = start;
     }
-    
+
     /**
      * @param timeOnRoad
      */
     public void setTimeOnRoad(int timeOnRoad) {
-	this.timeOnRoad = timeOnRoad;
+        this.timeOnRoad = timeOnRoad;
     }
-    
+
     /**
      * @param position
      */
     public void setPosition(SimpleImmutableEntry<Road, Double> position) {
-	this.position = position;
+        this.position = position;
     }
-    
+
     /**
      * @param desiredSpeed
      */
     public void setDesiredSpeed(double desiredSpeed) {
-	this.desiredSpeed = desiredSpeed;
+        this.desiredSpeed = desiredSpeed;
     }
-    
+
     /**
      * @param distance
      */
     public void setDistance(double distance) {
-	this.distance = distance;
+        this.distance = distance;
     }
-    
+
     /**
      * @param traceLog
      */
     public void setTraceLog(ArrayList<Intersection> traceLog) {
-	this.traceLog = traceLog;
+        this.traceLog = traceLog;
     }
-    
+
+    @Override
+    public void draw(Graphics g) {
+        SimpleImmutableEntry<Road, Double> position = getPosition();
+        Road road = position.getKey();
+        Intersection start = road.getStart();
+        Intersection end = road.getEnd();
+        Rectangle r = new Rectangle(4, 4);
+        
+    }
+
 }
