5276a9432f1e4e215de7a4691a607a266d9b2442
GQuest91 <E.boersma44@gmail.com>
2016-02-18 13:18:34 +0100

    Road getters andd setters

--- src/map/Road.java
+++ src/map/Road.java
@@ -1,5 +1,75 @@
 package map;
 
-public abstract class Road {
+import java.util.ArrayList;
+import java.util.Queue;
 
+public abstract class Road {
+	
+	private double maxSpeed;
+	/**
+	 * The speed limit in km/h
+	 */
+	private double kilometer;
+	/**
+	 * length of the road in kilometers
+	 */
+	private Intersection start;
+	/**
+	 * starting intersection of the road
+	 */
+	private Intersection end;
+	/**
+	 * ending intersection of the road
+	 */
+	private ArrayList<Road> neighbor;
+	/**
+	 * The roads that share the same start and end intersection
+	 */
+	
+	public void setMaxSpeed(double speed ) {
+		maxSpeed = speed;
+	}
+	
+	public void setLength(double kilometer) {
+		this.kilometer=kilometer;
+	}
+	
+	public void Start(Intersection start) {
+		this.start = start;
+	}
+	
+	public void End(Intersection end) {
+		this.end = end;
+	}
+	
+	public void AddNeighbor(Road road) {
+		if(this.start == road.getStart() && this.end == road.getEnd()) {
+			neighbor.add(road);
+		}
+			
+	}
+	
+	public double getMaxSpeed() {
+		return this.maxSpeed;
+	}
+
+	public double getLength() {
+		return this.kilometer;
+	}
+	
+	public Intersection getStart() {
+		return this.start;
+	}
+
+	public Intersection getEnd() {
+		return this.end;
+	}
+	
+	public Queue getQueue() {
+		return this.cars;
+	}
+	
+	public ArrayList <Road> getNeighbor() {
+		return this.neighbor;
+	}
 }
