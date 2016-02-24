e2c74113104fe28a8d36b82005fa57b946fb0993
imrayya <imrayya@gmail.com>
2016-02-24 09:16:39 +0100

    Changed model

--- src/models/Model.java
+++ src/models/Model.java
@@ -1,12 +1,14 @@
 package models;
 
+import vehicle.Vehicle;
+
 /**
  * Interface for all the models, Pipes,Forbes etc.
- * @author Lucas Vanparijs
- * @since 22-02-16
+ *
+ * @author Lucas Vanparijs, Kareem Horstink
  */
 public interface Model {
 
-    public void calculate(); //The calculations of each model go here, this will calculate how much the vehicle will move forward, may change when understanding of the model increases
+    public void calculate(Vehicle v);
 
 }
