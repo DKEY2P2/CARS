83363d3d386a59568b009a50fb670c188c35a1eb
imrayya <imrayya@gmail.com>
2016-03-03 13:43:01 +0100

    Fixed traffic light

--- src/map/intersection/TestIntersection.java
+++ src/map/intersection/TestIntersection.java
@@ -13,28 +13,25 @@ import java.util.AbstractMap;
 /**
  * Created by lvanp on 01/03/2016.
  */
-public class TestIntersection extends Intersection implements Observer{
+public class TestIntersection extends Intersection implements Observer {
 
     private Ticker ticker;
 
-    public TestIntersection(int x, int y, Ticker t){
+    public TestIntersection(int x, int y, Ticker t) {
         super(x, y);
         t.addObserver(this);
         ticker = t;
     }
 
-    public void update(){
+    public void update() {
         updateLight(ticker.getTimeBetweenTick());
-        for(TrafficLight tl : getTrafficLights()){
-            Vehicle v = tl.getIn().getVehicles().poll();
-            if(v ==null){
+        for (TrafficLight tl : getTrafficLights()) {
+            Vehicle v = tl.getWaiting().poll();
+            if (v == null) {
                 return;
             }
             v.addToTraceLog(this);
             Intersection placeToGo = v.nextPlaceToGo();
-            if(placeToGo == null){
-                Logger.LogHigh("The car doesn't know where it wants to go");
-            }
             Road r = null;
             for (Road road : getRoads()) {
                 if (road.getStart() == placeToGo) {
@@ -42,9 +39,8 @@ public class TestIntersection extends Intersection implements Observer{
                 }
             }
             if (r == null) {
-                Logger.LogError("Vehicle has no where to go", this);
                 for (Road road : getRoads()) {
-                    if(road.getEnd() != this){
+                    if (road.getEnd() != this) {
                         r = road;
                         break;
                     }
