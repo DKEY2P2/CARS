6e7468acaf34eb88e6f7c6f7e0f471f12d5b7ab0
imrayya <imrayya@gmail.com>
2016-03-03 23:22:40 +0100

    Made Forbe model better

--- src/models/Forbe.java
+++ src/models/Forbe.java
@@ -1,13 +1,8 @@
 package models;
 
 import controller.Controller;
-import helper.Timer;
-import java.util.AbstractMap;
 import java.util.AbstractMap.SimpleImmutableEntry;
-import java.util.ArrayList;
-
 import map.Road;
-import map.TrafficLight;
 import vehicle.Vehicle;
 
 /**
@@ -18,12 +13,6 @@ public class Forbe implements Model {
 
     private Vehicle getInFront(Vehicle veh, Road r) {
         Vehicle inFrontVehicle = null;
-//        int i = 0;
-//        i = r.getVehicles().indexOf(veh);
-//        if(i == r.getVehicles().size()-1){
-//            return null;
-//        }
-//        return r.getVehicles().get(i+1);
         for (Vehicle vehicle : r.getVehicles()) {
             if (vehicle == veh) {
                 break;
@@ -31,23 +20,11 @@ public class Forbe implements Model {
                 inFrontVehicle = vehicle;
             }
         }
-        //Get the car ahead of you
         return inFrontVehicle;
     }
 
     @Override
     public void calculate(Vehicle veh) {
-        if (veh.getIndex() == 1) {
-//            Timer.start();
-        }
-        Road r = veh.getPosition().getKey();
-        double roadLength = r.getLength();
-        double v = veh.getSpeed();
-        double l = veh.getLength();
-        double reactionTime = veh.getReactionTime();
-        double sMin = reactionTime * v + l;
-
-        Vehicle inFrontVehicle = getInFront(veh, r);
 
         //Waiting at the traffic light
         if (veh.getPosition().getKey() == null) {
@@ -57,18 +34,22 @@ public class Forbe implements Model {
         if (veh.getPosition().getValue() > 1) {
             return;
         }
-        //Get the road your on
 
+        Road r = veh.getPosition().getKey();
+        double roadLength = r.getLength();
+        double v = veh.getSpeed();
+        double l = veh.getLength();
+        double reactionTime = veh.getReactionTime();
+        double sMin = reactionTime * v + l;
+
+        Vehicle inFrontVehicle = getInFront(veh, r);
+
+        //Get the road your on
         //Get the percentage dunno why I split it when I split later on
         double[] percentage;
-        if (inFrontVehicle != null) {
-            percentage = new double[]{veh.getPosition().getValue(), inFrontVehicle.getPosition().getValue()};
-        } else {
-            percentage = new double[]{veh.getPosition().getValue(), 0};
-        }
-
         //If there is a car ahead of you
         if (inFrontVehicle != null) {
+            percentage = new double[]{veh.getPosition().getValue(), inFrontVehicle.getPosition().getValue()};
             //Get various of data
             double[] x = new double[]{roadLength * percentage[0], roadLength * percentage[1]};
             //Distance between the two cars
@@ -91,11 +72,14 @@ public class Forbe implements Model {
             //Update the position
             veh.setPosition(new SimpleImmutableEntry<>(r, newPercentage));
         } else {
+            percentage = new double[]{veh.getPosition().getValue(), 0};
             //Get various of data
             double x = roadLength * percentage[0];
 
             //Distance between you and the traffic light
             double s = Math.abs(x - roadLength);
+
+            sMin = reactionTime * v + veh.getDesiredSpeed() / (veh.getMaxDecceleration());
             //Time it takes you to react
             //If you can react fast enough
             if (s < sMin) {
@@ -106,16 +90,13 @@ public class Forbe implements Model {
                         Math.min(veh.getDesiredSpeed(), r.getSpeedLimit()),
                         v + veh.getMaxAcceleration() * Controller.getInstance().getTicker().getTickTimeInS());
             }
-                //Updates the speed
-                veh.setDistance(veh.getDistance() + v * Controller.getInstance().getTicker().getTickTimeInS());
-                double newPercentage = (x + v * Controller.getInstance().getTicker().getTickTimeInS()) / roadLength;
-                veh.setSpeed(v);
-                //Update the position
-                veh.setPosition(new SimpleImmutableEntry<>(r, newPercentage));
+            //Updates the speed
+            veh.setDistance(veh.getDistance() + v * Controller.getInstance().getTicker().getTickTimeInS());
+            double newPercentage = (x + v * Controller.getInstance().getTicker().getTickTimeInS()) / roadLength;
+            veh.setSpeed(v);
+            //Update the position
+            veh.setPosition(new SimpleImmutableEntry<>(r, newPercentage));
 
-        }
-        if (veh.getIndex() == 1) {
-//            Timer.nanoPrint();
         }
     }
 
