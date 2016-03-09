4879f9737904c65e9c9c7a2d10495d1f1e017e49
Jonas Vacek <jvacek@me.com>
2016-03-09 12:02:06 +0100

    no message

---
+++ src/sandbox/JSON_reader.java
@@ -0,0 +1,41 @@
+package sandbox;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import controller.Controller;
+import controller.Ticker;
+import json.GEOjson;
+import map.Map;
+
+public class JSON_reader {
+    
+    public static void main(String[] args) throws FileNotFoundException {
+	Controller ctrl = Controller.getInstance();
+	ctrl.setTicker(new Ticker(1, 1));
+	
+	
+	// TODO Auto-generated method stub
+	String filepath = "resources/geojson/test.json";
+	
+	boolean debug = true;
+	if (debug) {
+	    File file = new File(filepath);
+	    Scanner scanner = new Scanner(file);
+	    String text = scanner.useDelimiter("\\A").next();
+	    scanner.close(); // Put this call in a finally block
+//	    System.out.println("File " + text + "\n");
+	}
+	
+	Map map = GEOjson.GEOJsonConverter(filepath);
+	System.out.println(map);
+	
+    }
+ 
+    
+    
+    
+    
+    
+}
