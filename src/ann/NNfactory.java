cf80a45335a919b59eaca3b874fd915d37968d80
imrayya <imrayya@gmail.com>
2016-04-16 18:10:05 +0200

    The start of the ANN

---
+++ src/ann/NNfactory.java
@@ -0,0 +1,10 @@
+package ann;
+
+/**
+ * The factory that creates the neural network
+ *
+ * @author Kareem Horstink
+ */
+public class NNfactory {
+    
+}
---
+++ src/ann/TesterClass.java
@@ -0,0 +1,11 @@
+package ann;
+
+/**
+ * A class to test out the ANN (artifical neural network). To be later plugged
+ * into the real simulation
+ *
+ * @author Kareem
+ */
+public class TesterClass {
+
+}
