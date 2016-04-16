5318b6449a5c708d4bd0c65b55118d38f8517963
imrayya <imrayya@gmail.com>
2016-04-16 20:01:13 +0200

    Comment were made to make it easier to understand..... Doesn't really help xD

--- src/ann/MultilayerPerceptron.java
+++ src/ann/MultilayerPerceptron.java
@@ -16,6 +16,9 @@ import java.util.Random;
  */
 public class MultilayerPerceptron implements NeuralNetwork {
 
+    /**
+     * For creating the random weights
+     */
     private static final Random RANDOM = new Random(24031995);
 
     // [layer][nodeNumber]
@@ -25,53 +28,61 @@ public class MultilayerPerceptron implements NeuralNetwork {
     private double[][][] weights;
 
     protected MultilayerPerceptron(int inputLayer, int[] hiddenLayer, int outputLayer) {
+        /* The creation of the nodes */
         ArrayList<Node[]> tmp = new ArrayList();
 
         for (int i = 0; i < hiddenLayer.length + 2; i++) {
-            if (i == 0) {
+            if (i == 0) {//input layer
                 tmp.add(new Node[inputLayer]);
-
-            } else if (i == hiddenLayer.length + 1) {
+            } else if (i == hiddenLayer.length + 1) { //out layer
                 tmp.add(new Node[outputLayer]);
                 break;
-            } else {
+            } else {//every other layer
                 tmp.add(new Node[hiddenLayer[i - 1]]);
             }
         }
 
-        nodes = tmp.toArray(new Node[1][1]);
+        nodes = tmp.toArray(new Node[1][1]); //make it into a "array" 
 
+        /* Creates the nodes */
         for (Node[] node : nodes) {
             for (int j = 0; j < node.length; j++) {
                 node[j] = new Node();
             }
         }
+
+        /*Creation of the weights*/
         generateRandomWeights();
     }
 
+    /**
+     * Generates the structure of the weights and set the weights to some random
+     * numbers
+     */
     private void generateRandomWeights() {
-        ArrayList<double[]> weightLayer = new ArrayList<>();
-        ArrayList<double[][]> tmp = new ArrayList<>();
+        /*Don't ask me how to explain this, it works and I already forgotten
+         how the is worked and I wrote it like 5 mins ago ~ Kareem*/
+        ArrayList<double[]> nodeWeight = new ArrayList<>();
+        ArrayList<double[][]> layer = new ArrayList<>();
 
+        //Magic
         for (int i = 0; i < nodes.length - 1; i++) {
             Node[] nodeLayer = nodes[i];
-            for (int j = 0; j < nodeLayer.length; j++) {
-                Node superCurrent = nodeLayer[j];
-                weightLayer.add(new double[nodes[i + 1].length]);
+            for (Node nodeLayer1 : nodeLayer) {
+                nodeWeight.add(new double[nodes[i + 1].length]);
             }
-            tmp.add(weightLayer.toArray(new double[1][1]));
-            weightLayer.clear();
+            layer.add(nodeWeight.toArray(new double[1][1]));
+            nodeWeight.clear();
         }
-        weights = tmp.toArray(new double[1][1][1]);
+        weights = layer.toArray(new double[1][1][1]);
 
+        /* Creates the random weights */
         for (double[][] weight : weights) {
             for (double[] weight1 : weight) {
                 for (int i = 0; i < weight1.length; i++) {
                     weight1[i] = RANDOM.nextDouble();
-
                 }
             }
-
         }
     }
 
@@ -87,26 +98,36 @@ public class MultilayerPerceptron implements NeuralNetwork {
 
     @Override
     public double[] activate(double[] input) {
+        /* Checks the input */
         if (input.length != nodes[0].length) {
             throw new IllegalArgumentException("The amount of input does not reflect the amount of input nodes");
         }
 
+        /* Set the input nodes value to the new values */
         for (int i = 0; i < input.length; i++) {
             nodes[0][i].setCurrentValue(input[i]);
         }
 
+        /* Calculates the rest of the values */
         for (int layer = 1; layer < nodes.length; layer++) {
-            Node[] currentLayer = nodes[layer];
-            Node[] previousLayer = nodes[layer - 1];
+
+            Node[] currentLayer = nodes[layer];//The current layer that the program is updating
+            Node[] previousLayer = nodes[layer - 1]; //The previous layer that is used to update the current layer
+
             for (int nodeNumber = 0; nodeNumber < currentLayer.length; nodeNumber++) {
-                Node node1 = currentLayer[nodeNumber];
+                Node node1 = currentLayer[nodeNumber]; //Which node we are updating
+
                 double sum = 0;
+                //Get the sum of all the input to the current node
                 for (int i = 0; i < previousLayer.length; i++) {
                     sum += previousLayer[i].getCurrentValue() * weights[layer - 1][i][nodeNumber];
                 }
+                //Set the current value of the node to activation based on the input
                 node1.setCurrentValue(activationFunction(sum));
             }
         }
+        
+        //Get the results based on the output layer nodes
         double[] results = new double[nodes[nodes.length - 1].length];
         for (int i = 0; i < nodes[nodes.length - 1].length; i++) {
             results[i] = nodes[nodes.length - 1][i].getCurrentValue();
@@ -116,6 +137,7 @@ public class MultilayerPerceptron implements NeuralNetwork {
 
     @Override
     public void printWeights() {
+        //Prints the weights all "pretty" like
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < weights.length; i++) {
             builder.append(Integer.toString(i));
--- src/ann/NNfactory.java
+++ src/ann/NNfactory.java
@@ -6,7 +6,15 @@ package ann;
  * @author Kareem Horstink
  */
 public class NNfactory {
-    
+
+    /**
+     * Creates Multilayer Perceptron
+     *
+     * @param inputLayer The amount of input nodes wanted
+     * @param hiddenLayer The amount of nodes wanted for each hidden layer
+     * @param outputLayer The amount of output nodes
+     * @return The the NN
+     */
     public static NeuralNetwork createMultilayerPerceptron(int inputLayer, int[] hiddenLayer, int outputLayer) {
         return new MultilayerPerceptron(inputLayer, hiddenLayer, outputLayer);
     }
